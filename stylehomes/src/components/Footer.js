e6877133d8e3e5b6a0bcafe8b97d08bfac48590e
Alan dos Anjos Farias <farias_alan@outlook.com>
2020-10-18 23:35:32 -0300

    add objetos

---
+++ stylehomes/src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = styled.footer `
+position: absolute;
+bottom: 0;
+left: 0;
+height: 40px;
+weidth: 100%;
+display:flex;
+justify-content: center;
+align-items: center;
+backgroun-color: #8793;
+height: 90px;
+`
+
+const FooterItem = styled.div `
+text-decoration: none;
+font-size: 16px;
+color: #000;
+font-weight: bold;
+boittom: 0;
+`
+
+export default function footer(){
+    return(
+        <React.Fragment>
+            <Footer>
+                <FooterItem>Aula Sobre Styled components</FooterItem>
+            </Footer>
+        </React.Fragment>
+    );
+}
